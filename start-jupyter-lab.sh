499cec6af0caa86af143d7600d28f9b1b2c41151
Seb Ospina <kraige@gmail.com>
2025-08-20 21:51:17 +0200

    Missing dollar sign on passwd

--- start-jupyter-lab.sh
+++ start-jupyter-lab.sh
@@ -2,4 +2,4 @@
 
 set -xe
 source /home/seb/git/s2-polars-data-analysis/venv/bin/activate
-jupyter lab --ip=0.0.0.0 --no-browser --port=8888 --NotebookApp.token='' --NotebookApp.password="JPT_PASSWD" --NotebookApp.disable_check_xsrf=True --notebook-dir=/home/seb/git/s2-polars-data-analysis/jupyter_notebooks/
+jupyter lab --ip=0.0.0.0 --no-browser --port=8888 --NotebookApp.token='' --NotebookApp.password="$JPT_PASSWD" --NotebookApp.disable_check_xsrf=True --notebook-dir=/home/seb/git/s2-polars-data-analysis/jupyter_notebooks/
